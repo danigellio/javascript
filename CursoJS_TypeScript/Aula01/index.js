f8021eb3a13b45e19b8a7fcd00b1e34201359559
Daniela Gellio <danigeliodias@hotmail.com>
2023-10-26 23:45:04 -0300

    aulas inicias

---
+++ CursoJS_TypeScript/Aula01/index.js
@@ -0,0 +1,7 @@
+console.log('Daniela'); //String
+console.log("Daniela");
+console.log(`Daniela`);
+
+console.log(123456);
+console.log(123456, 12345.678, 'daniela');
+//tudo é Number, nao tem diferença entre int ou float
---
+++ CursoJS_TypeScript/Aula02/index.js
@@ -0,0 +1,14 @@
+console.log('Meu nome é "Daniela". Estou aprendendo JavaScript às ',23,'da noite.')
+//colcoar , no numero,
+//Modo de comentário
+
+/**
+ * um titulo
+ * lorem ipsum
+ * 
+ */
+
+//linha
+
+/*comentario
+comentario*/
