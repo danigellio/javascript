85935f7e0d626267207f9790b878db46bc700916
Daniela Gellio <danigeliodias@hotmail.com>
2024-02-29 16:53:45 +0000

    desafio01 e  operadores de comparação
    Finalização do desafio01 e aula sobre operadores de comparação

---
+++ CursoJS_TypeScript/Aula 37- Operadores de comparação/index.js
@@ -0,0 +1,19 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual
+ * == igualdade ///valor --- nao recomendado
+ * === igualdade estrita/// valor e tipo
+ * != diferente //valor --- nao recomendado
+ * !== diferente estrito /// valor e tipo
+ */
+const comparação = 10 > 5;
+console.log( 10 > 5);
+
+const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 === num2; 
+console.log(comp);
+
--- CursoJS_TypeScript/Desafios/Desafio 01/index.js
+++ CursoJS_TypeScript/Desafios/Desafio 01/index.js
@@ -1,31 +1,45 @@
-/*function meuEscopo (){
-     const form = document.querySelector('.form');
-      // se eu quiser selecionar uma classe eu coloco ('.FORM') e id ('#form')
-      const resultado = document.querySelector( '.resultado');
+function meuEscopo (){
+  const form = document.querySelector('.form');
+  const res = document.querySelector('.resultado');
+  const pessoas = []; // o array esta vazio pq o que vai preencher sera oq enviamos no formulario
+ 
+  function recebeEventoForm(evento){
+    evento.preventDefault();
 
 
-    
-      const pessoas = [];
+    //seleciona os input
+    const nome = form.querySelector('.nome'); 
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura =  form.querySelector('.altura');
 
-    // form.onsubmit = function (evento){ //onsubit é onde eu qero receber a ação... on significa alguma funcao evente
-         // evento.preventDefault();
-       //   alert(1);
 
-     function recebeEventoForm (evento){
-      evento.preventDefault();
+    //console.log(nome.value,sobrenome,peso,altura);//com value eu pego os valores ex: nome.value
 
-          const nome = form.querySelector('.nome');
-          const sobrenome = form.querySelector('.sobrenome');
-          const peso = form.querySelector('.peso');
-          const altura = form.querySelector('.altura');
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
+    console.log(pessoas);
 
-        console.log(nome,sobrenome,peso,altura);
-     }
+    //para mostrar no Resultado
+    res.innerHTML += `<p> ${nome.value} ${sobrenome.value} `+`${peso.value} ${altura.value}<p>`;
+    }
 
-          
-       form.addEventListener('submit', recebeEventoForm); // forma mais moderna, de escutar o evente
-     
+
+  form.addEventListener('submit', recebeEventoForm); 
+  //forma mais moderna, de escutar o evente, adiciona um escutador de evento
 }
-meuEscopo();*/
+
+meuEscopo();
+
+//VARIAVEIS DENTRO DA FUNÇÃO SAO SOMENTE DENTRO DA FUNÇÃO, E  NAO GLOBAL
+//se eu quiser selecionar uma classe eu coloco ('.FORM') e id ('#form')
+// form.onsubmit = function (evento){ //onsubit é onde eu qero receber a ação... on significa alguma funcao evente
+ // evento.preventDefault(); //diz para o navegador se previnir  oq era p acontecer por padrao e nao envia o formulario
+
+     
 
 
